87b27c6579ad4ae336149388d4bf5c4ec343b7c8
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-04-12 20:36:54 -0600

    first commit

---
+++ src/main/java/com/ocean/accounting/OceanAccountingServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanAccountingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanAccountingServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ocean/accounting/OceanAccountingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanAccountingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
