69d5bff2783538bf73e51071769539c817e1c3c6
Augustine.Oseafiana <augustine.oseafiana@gmail.com>
2023-04-23 21:41:23 -0600

    AccountingDao, AccountingService

--- src/main/java/com/ocean/accounting/dao/OceanAccountingDao.java
+++ src/main/java/com/ocean/accounting/dao/OceanAccountingDao.java
@@ -3,6 +3,6 @@ package com.ocean.accounting.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class AccountingzDao {
+public class OceanAccountingDao {
 
 }
--- src/main/java/com/ocean/accounting/service/OceanAccountingService.java
+++ src/main/java/com/ocean/accounting/service/OceanAccountingService.java
@@ -3,6 +3,6 @@ package com.ocean.accounting.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AccountingService {
+public class OceanAccountingService {
 
 }
